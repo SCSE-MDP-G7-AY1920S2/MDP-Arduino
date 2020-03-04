76f036867db5584a2271468ae150fdb174d10526
hengkaiz <57538939+hengkaiz@users.noreply.github.com>
2020-03-04 15:38:41 +0800

    deploy for windows.

---
+++ deploy_win.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Sync workspace.
+arduino-cli compile --fqbn arduino:avr:uno arduinoMain
+scp -r arduinoMain rpi:Arduino/
+# Compile and upload the sketch.
+ssh rpi "source .profile && cd Arduino && arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno arduinoMain"
