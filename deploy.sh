d3cd4dddb719823feaa991c4aafaf621fe4c6496
Kaishuo Zhang <6542131+zhangks98@users.noreply.github.com>
2020-03-04 14:19:34 +0800

    Change deploy to using rsync

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Sync workspace.
 arduino-cli compile --fqbn arduino:avr:uno arduinoMain
-scp -r arduinoMain rpi:Arduino/
+rsync -avz arduinoMain rpi:Arduino/
 # Compile and upload the sketch.
 ssh rpi "source .profile && cd Arduino && arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno arduinoMain"
 
